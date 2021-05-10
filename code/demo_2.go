80775674e212acfdbf7d6d4c03492afa5b03bef5
NaraLuwan <hqcadmin@163.com>
2021-05-10 20:30:28 +0800

    add:基础语法

--- code/demo_2.go
+++ code/demo_2.go
@@ -28,4 +28,36 @@ func main(){
 
     arr_8 :=  [...][5] int {{1, 2, 3, 4, 5}, {9, 8, 7, 6, 5}, {0:3, 1:5, 4:6}}
     fmt.Println(arr_8)
+
+    // 长度不可变，运行会报错：invalid array index 4 (out of bounds for 3-element array)
+    var arr_9 = [3] int {1, 2, 3}
+    arr_9[4] = 4    // 去掉这行就可以
+    fmt.Println(arr_9)
+
+    // 数组是值类型问题，在函数中传递的时候是传递的值，如果传递数组很大，这对内存是很大开销
+    // 运行输出[1, 2, 3, 4, 5]
+    var arr_10 =  [5] int {1, 2, 3, 4, 5}
+	modifyArr(arr_10)
+	fmt.Println(arr_10)
+
+	// 运行输出[1, 20, 3, 4, 5]
+	var arr_11 =  [5] int {1, 2, 3, 4, 5}
+    modifyArr2(&arr_11)
+    fmt.Println(arr_11)
+
+    // 数组赋值问题，同样类型的数组（长度一样且每个元素类型也一样）才可以相互赋值，反之不可以
+    // 运行会报错：cannot use arr_12 (type [5]int) as type [6]int in assignment
+    var arr_12 =  [5] int {1, 2, 3, 4, 5}
+    var arr_13 [5] int = arr_12
+    var arr_14 [6] int = arr_12    // 去掉这行就可以
+    fmt.Println(arr_13)
+    fmt.Println(arr_14)    // 去掉这行就可以
+}
+
+func modifyArr(arr [5] int) {
+	arr[1] = 20
+}
+
+func modifyArr2(arr *[5] int) {
+	arr[1] = 20
 }
