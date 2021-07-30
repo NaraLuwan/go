394d257e5a81b36897a105aa3cbf8c46e637ba18
NaraLuwan <hqcadmin@163.com>
2021-07-30 17:24:26 +0800

    ADD:json marshal and unmarshal

--- code/demo_12.go
+++ code/demo_12.go
@@ -23,6 +23,18 @@ type Class struct {
 	Grade int
 }
 
+/**
+上面的成员变量都是已知的类型，只能接收指定的类型，比如string类型的Name只能赋值string类型的数据
+但有时为了通用性，或使代码简洁，我们希望有一种类型可以接受各种类型的数据，并进行json编码。这就用到了interface{}类型
+*/
+type Stu2 struct {
+	Name  interface{} `json:"name"`
+	Age   interface{}
+	HIgh  interface{}
+	sex   interface{}
+	Class interface{} `json:"class"`
+}
+
 func main() {
 	// 实例化一个结构
 	stu := Stu{
@@ -48,4 +60,20 @@ func main() {
 	}
 
 	fmt.Println(string(jsonStu))
+
+	// 实例化一个结构
+	stu2 := Stu{
+		Name: "Tom",
+		Age:  18,
+		High: true,
+		sex:  "男",
+	}
+	stu2.Class = cla
+
+	jsonStu2, err := json.Marshal(stu2)
+	if err != nil {
+		fmt.Println("序列化错误")
+	}
+
+	fmt.Println(string(jsonStu2))
 }
