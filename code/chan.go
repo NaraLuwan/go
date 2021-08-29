03c495492a2fbd188fa0e17c3e89c1ffee94085b
NaraLuwan <hqcadmin@163.com>
2021-08-29 23:32:11 +0800

    ADD:chan

---
+++ code/chan.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	naturals := make(chan int)
+	squares := make(chan int)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			naturals <- i
+		}
+		close(naturals)
+	}()
+
+	go func() {
+		for x := range naturals {
+			squares <- x * x
+		}
+		close(squares)
+	}()
+
+	for x := range squares {
+		fmt.Println(x)
+	}
+}
