ee27efa30d2f42706d168ae49853ab89482502cf
NaraLuwan <hqcadmin@163.com>
2021-11-29 15:21:26 +0800

    ADD:pipeline

---
+++ code/pipeline.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func main() {
+	// Set up the pipeline and consume the output.
+	for n := range sq(sq(gen(2, 3))) {
+		fmt.Println(n)
+	}
+}
