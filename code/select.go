21805c5377eec679d8a9d1b0741df46f52fffcd9
NaraLuwan <hqcadmin@163.com>
2022-01-16 20:08:53 +0800

    ADD:select

---
+++ code/select.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// 多路复用
+func main() {
+	ch := make(chan int, 1)
+	for i := 0; i < 10; i++ {
+		select {
+		case x := <-ch:
+			fmt.Println(x)
+		case ch <- i:
+		}
+	}
+}
