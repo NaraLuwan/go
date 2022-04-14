f8b46cc67519b42044127afbeea8170fb578268a
breatche <breatche@tencent.com>
2022-04-14 19:57:49 +0800

    feat: timeAfter

---
+++ code/timeafter.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func main() {
+	for i := 0; i < 1000; i++ {
+		timeoutWithBuffer(doBadThing)
+	}
+	time.Sleep(time.Second * 2)
+	fmt.Println(runtime.NumGoroutine())
+}
+
+func timeoutWithBuffer(f func(chan bool)) error {
+	done := make(chan bool, 1)
+	go f(done)
+	select {
+	case <-done:
+		fmt.Println("done")
+		return nil
+	case <-time.After(time.Millisecond):
+		return fmt.Errorf("timeout")
+	}
+}
+
+func doBadThing(done chan bool) {
+	time.Sleep(time.Second)
+	done <- true
+}
